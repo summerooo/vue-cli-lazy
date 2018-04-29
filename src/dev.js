0b1d4a569af6bc56a01b172d515df412368db903
WrenchDE <1695408430@qq.com>
2018-04-29 22:53:08 +0800

    go

--- src/dev.js
+++ src/dev.js
@@ -1,16 +1,29 @@
-
-// 开发环境为true，生产环境为false
+// 0 dev 1 build 2 App
+let what = 0
+// what = 1
+// what = 2
+// axios 的 baseURL
 let isDev = true
-// let isDev = false
-
+let isApp = false
 let apiUrl = ''
-let host = 'http://www.udao56.com/thinkphp5/index.php/tms'
+// 代理target
+let host = 'http://test.klqzz.top/'
 
-if (isDev) apiUrl = '/api'
-else apiUrl = host
-
+switch (what) {
+  case 0:
+    apiUrl = '/api'
+    break
+  case 1:
+    apiUrl = host = ''
+    break
+  case 2:
+    apiUrl = host
+    isApp = true
+    break
+}
 module.exports = {
   isDev,
+  isApp,
   host,
   apiUrl
 }
