824836f1a50f353b0551ad51a6c586d45c00be13
WrenchDE <1695408430@qq.com>
2018-06-04 18:38:37 +0800

    nginx配置

--- src/api/a/a.js
+++ src/api/a/a.js
@@ -1,9 +1,11 @@
 import axios from '../index'
-import {api} from '@/dev'
+const {api, hot} = require('@/dev').proxyTableApi
+console.log(api, hot)
+
 export const login = data => {
   return axios.post(api + '/login/verify', data)
 }
 
 export const hotMapData = data => {
-  return axios.get('/hot/data/asset/data/hangzhou-tracks.json', data)
+  return axios.get(hot + '/data/asset/data/hangzhou-tracks.json', data)
 }
--- src/dev.js
+++ src/dev.js
@@ -1,9 +1,16 @@
-
+// ----------- 以下默认
+//  nginx 和 gzip
+//  gzip 是 因为cordova app 不支持gzip
+// ------------
+// 服务主要地址
+let host = 'http://www.udao56.com/thinkphp5/index.php/tms'
+// Is your service nginx  和 npm run dev 都是代理方式
+let nginx = false
+nginx = true
 // 是APP是 gzip 选用 false
 let gzip = true
 // gzip = false
-let host = 'http://www.udao56.com/thinkphp5/index.php/tms'
-// ----------- 以下默认
+// ----------------------------------------------------
 let proxyTable = {
   '/api': {
     target: host,
@@ -12,7 +19,7 @@ let proxyTable = {
       '^/api': '/'
     }
   },
-  // 仅测试可用，因build 后放到服务器环境是没有代理
+  // 没有代理服务器环境 不可用, 用上nginx代理 可用
   '/hot': {
     target: 'http://echarts.baidu.com/',
     changeOrigin: true,
@@ -21,15 +28,31 @@ let proxyTable = {
     }
   }
 }
-let api = '/api'
-if (process.env.NODE_ENV === 'development') {
-  api = '/api'
-} else {
-  api = ''
-  if (!gzip) api = host
+// nginx 相对的配置   我把nginx 文件放到了 static 下  名字 为nginx.conf 具体配置可以看
+
+// location /api {
+//   proxy_pass http://www.udao56.com/thinkphp5/index.php/tms;
+// }
+// location /hot {
+//     proxy_pass http://echarts.baidu.com/;
+// }
+
+let proxyTableApi = {}
+// Is your service nginx  和 npm run dev 都是代理方式
+// 封装一个对象，如 {'api': '/api’}
+for (let i in proxyTable) {
+  proxyTableApi[i.replace(/\//, '')] = i
+}
+// build 后 无代理 放到指定服务目录下 无代理 所以不能用 /hot...
+if (process.env.NODE_ENV !== 'development') {
+  if (!nginx) {
+    for (let i in proxyTable) {
+      proxyTableApi[i.replace(/\//, '')] = ''
+    }
+  }
 }
 module.exports = {
   proxyTable,
-  gzip,
-  api
+  proxyTableApi,
+  gzip
 }
