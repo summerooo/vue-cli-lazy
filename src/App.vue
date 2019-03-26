7189265aac1d3cf0e547f203c1e012c6fe46839d
WrenchDE <1695408430@qq.com>
2019-03-26 10:24:23 +0800

    add style
    add style

--- src/App.vue
+++ src/App.vue
@@ -35,4 +35,24 @@ export default {
 </script>
 
 <style lang="scss">
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+#app,
+html,
+body {
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration:none;
+}
 </style>
