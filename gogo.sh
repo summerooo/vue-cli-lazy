8be4961d43883063ff73931e8fd8349b10ef6e88
summerooo <1695408430@qq.com>
2020-11-21 16:31:50 +0800

    desc

--- gogo.sh
+++ gogo.sh
@@ -1,4 +1,10 @@
-git pull
+
+git pull origin master
 git add .
-git commit -m 'update'
-git push
+if [ -n "$1" ]; then
+  desc=$1
+else
+  desc='update'
+fi
+git commit -m desc
+git push origin master
