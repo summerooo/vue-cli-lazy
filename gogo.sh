63bf3dd3f6feae1ef89c224789d673785073d297
macbook <macbook@huogun.local>
2025-07-08 17:29:15 +0800

    update

--- gogo.sh
+++ gogo.sh
@@ -1,14 +1,14 @@
 if [ "$2" == "build" ]; then
 yarn build
 fi
-git pull origin master
+git pull
 git add .
 if [ -n "$1" ]; then
   git commit -m $1
 else
   git commit -m 'update'
 fi
-git push origin master
+git push
 if [ "$2" == "build" ]; then
   # yarn build
   # expect expect.sh
