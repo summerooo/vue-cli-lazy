352b7240a24b6d799c34e993bae36f223c2f3cb9
macbook <macbook@huogun.local>
2026-03-03 09:36:31 +0800

    增加gzip

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,50 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { proxy } from './src/dev'
 import path from 'path'
+import viteCompression from 'vite-plugin-compression'
 
-export default defineConfig({
-  define: {
-    'process.env': process.env
-  },
-  plugins: [vue()],
-  server: {
-    proxy,
-    host: true
-  },
-  esbuild: {
-    drop: ['console', 'debugger'],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '/@/': path.resolve(__dirname, 'src')
+export default defineConfig(({ mode }) => {
+  // 加载当前模式的环境变量
+  const env = loadEnv(mode, process.cwd(), '')
+  // 注入到 process.env 供 src/dev.js 使用
+  Object.assign(process.env, env)
+
+  return {
+    define: {
+      'process.env': env
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        charset: false,
-        // additionalData: `@use "~/styles/element/index.scss" as *;`,
-        // additionalData: `@import "./src/assets/css/common.scss";`
-        additionalData: `@use "./src/assets/css/common.scss" as *;`
-      }
+    plugins: [
+      vue(),
+      viteCompression({
+        threshold: 10240, // 超过 10kb 则压缩
+        algorithm: 'gzip', // 压缩算法
+        ext: '.gz', // 文件后缀
+        compressionOptions: {
+          level: 9,               // 压缩级别 1-9，数字越大压缩率越高
+        },
+      })
+    ],
+    server: {
+      proxy,
+      host: true
     },
+    esbuild: {
+      drop: ['console', 'debugger'],
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '/@/': path.resolve(__dirname, 'src')
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          charset: false,
+          additionalData: `@use "./src/assets/css/common.scss" as *;`
+        }
+      },
+    }
   }
 })
